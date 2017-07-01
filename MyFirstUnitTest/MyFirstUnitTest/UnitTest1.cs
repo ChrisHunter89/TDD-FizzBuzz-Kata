92a08aea906477250eabe8e9fa39b27a4cdc76cc
Chris Hunter <chris-2k6@hotmail.co.uk>
2017-07-01 21:53:54 +0100

    Commit FizzBuzz
    Commit FizzBuzz TDD Kata.

---
+++ MyFirstUnitTest/MyFirstUnitTest/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyFirstUnitTest
+{
+    [TestClass]
+    public class TestingFizzBuzzCalculator
+    {
+        FizzBuzz fizzBuzz;
+
+        [TestInitialize]
+        public void FizzBuzzInitialize()
+        {
+            fizzBuzz = new FizzBuzz();
+        }
+
+        [TestMethod]
+        public void Given1ShouldReturn1()
+        {
+            var result = fizzBuzz.Calculate(1);
+            Assert.AreEqual(result, "1");
+        }
+
+        [TestMethod]
+        public void Given2ShouldReturn2()
+        {
+            var result = fizzBuzz.Calculate(2);
+            Assert.AreEqual(result, "2");
+        }
+
+        [TestMethod]
+        public void Given1ShouldReturnFizz()
+        {
+            var result = fizzBuzz.Calculate(3);
+            Assert.AreEqual(result, "Fizz");
+        }
+
+        [TestMethod]
+        public void Given5ShouldReturnBuzz()
+        {
+            var result = fizzBuzz.Calculate(5);
+            Assert.AreEqual(result, "Buzz");
+        }
+
+        [TestMethod]
+        public void Given6ShouldReturnBuzz()
+        {
+            var result = fizzBuzz.Calculate(6);
+            Assert.AreEqual(result, "Fizz");
+        }
+
+        [TestMethod]
+        public void Given10ShouldReturnBuzz()
+        {
+            var result = fizzBuzz.Calculate(10);
+            Assert.AreEqual(result, "Buzz");
+        }
+
+        [TestMethod]
+        public void Given15ShouldReturnFizzBuzz()
+        {
+            var result = fizzBuzz.Calculate(15);
+            Assert.AreEqual(result, "FizzBuzz");
+        }
+    }
+}
+
+public class FizzBuzz
+{
+    public string Calculate(int number)
+    {
+        var fizzBuzzResult = "";
+
+        if (number % 3 == 0)
+        {
+            fizzBuzzResult = "Fizz";
+        }
+
+        if (number % 5 == 0)
+        {
+            fizzBuzzResult = fizzBuzzResult + "Buzz";
+        }
+
+        return string.IsNullOrEmpty(fizzBuzzResult) ? number.ToString() : fizzBuzzResult;
+    }
+}
